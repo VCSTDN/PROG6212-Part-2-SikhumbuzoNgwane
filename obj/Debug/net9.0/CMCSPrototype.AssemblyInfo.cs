4134ec0040fcdc4c4e35a1befc2180ec441c20c4
Sikhumbuzo <ST10438946@vcconnect.edu.za>
2025-09-17 20:17:11 +0200

    Commit 2: Updated Home/Index view with Bootstrap buttons

--- obj/Debug/net9.0/CMCSPrototype.AssemblyInfo.cs
+++ obj/Debug/net9.0/CMCSPrototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCSPrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a432f17d4ca40f077b1b8e5d941c53e3679276aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCSPrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCSPrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
